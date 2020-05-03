e0ff0856fc3e2bae893778a3f700dccae68ae817
jadiunr <jadiunr@fout.jp>
2020-05-03 17:37:52 +0900

    何で今までsince_idを使ってこなかったんだ？

--- main.pl
+++ main.pl
@@ -26,26 +26,23 @@ my $slack = Slack::WebAPI->new(
 
 say 'Initialize now.';
 # Initialize
-my $old_tweet_ids = [];
 my $tweets = $nt->user_timeline({user_id => $ENV{COVFEFE_TWITTER_TARGET_ID}, count => 30});
-push(@$old_tweet_ids, $_->{id}) for @$tweets;
+my $since_id = $tweets->[0]{id};
 
 say 'Capture begin.';
 # Crawling routine
-while (1) {
-  my $tmp_tweets = eval { $nt->user_timeline({user_id => $ENV{COVFEFE_TWITTER_TARGET_ID}, count => 30}) };
+while(1) {
+  $tweets = eval { $nt->user_timeline({user_id => $ENV{COVFEFE_TWITTER_TARGET_ID}, count => 30, since_id => $since_id}) };
   warn "WARNING: $@" and next if $@;
-  $tweets = $tmp_tweets;
-  for my $i (reverse 0..5) {
-    unless (grep {$_ eq $tweets->[$i]{id}} @$old_tweet_ids) {
-      $slack->notify($tweets->[$i]);
-      my $media = $tweets->[$i]{extended_entities}{media};
+
+  if (@$tweets) {
+    for my $tweet (@$tweets) {
+      $slack->notify($tweet);
+      my $media = $tweet->{extended_entities}{media};
       $slack->upload($media) if $media;
     }
+    $since_id = $tweets->[0]{id}
   }
-  my $latest_tweet_ids = [];
-  push(@$latest_tweet_ids, $_->{id}) for @$tweets;
-  $old_tweet_ids = $latest_tweet_ids;
 
-  sleep(2);
+  sleep 2;
 }
